6653a063e1dfc1ed42557134956cf6ab6cb800d1
Mahmoud Hafez <Mahmoud.hafez@ihorizons.com>
2025-04-14 22:49:47 +0200

    init pro

---
+++ VersionControl/VersionControlApp.swift
@@ -0,0 +1,19 @@
+//
+//  VersionControlApp.swift
+//  VersionControl
+//
+//  Created by Mahmoud Hafez on 14/04/2025.
+//
+ // 1
+// KHALED
+// BASSEM
+import SwiftUI
+
+@main
+struct VersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
